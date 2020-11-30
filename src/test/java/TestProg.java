28ec98ca04c4ddaf2bbfbeb00bc1ed12c7240c30
maxim-creator <max47166978@gmail.com>
2020-11-30 09:08:53 +0300

    1 more test

--- src/test/java/TestProg.java
+++ src/test/java/TestProg.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestProg {
-Main main = new main();
+    Main main = new Main();
+
     @Test
     public void testReadData() throws FileNotFoundException {
         Main main = new Main();
@@ -20,13 +22,18 @@ Main main = new main();
         assertEquals(test2, test);
     }
 
-@Test
-public void testSearchBiggestPalindrome(){
-List<String> data = new ArrayList();
-{
-data.add("qw wq");
-}
-assertEquals(5, main.searchBiggestPalindrome(data));
-}
+    @Test
+    public void testSearchBiggestPalindrome() {
+        List<String> data = new ArrayList();
+        {
+            data.add("qw wq");
+        }
+        assertEquals(5, main.searchBiggestPalindrome(data));
+    }
+
+    @Test
+    public void testIsPalindrome(){
+        assertTrue(main.isPalindrome("qwwq"));
+    }
 
 }
